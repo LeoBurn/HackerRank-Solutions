916cdf95f2e61ba81a71ec595b4867ad2542051a
LeandroViana <leandroviana1988@gmail.com>
2017-07-30 16:10:53 +0100

    Base Of Repository

---
+++ HackerRankSolutions/HackerRankSolutions.Algorithms/Implementation/BeautifulDaysAtTheMovies.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.Algorithms.Implementation
+{
+    public class BeautifulDaysAtTheMovies
+    {
+    }
+}
