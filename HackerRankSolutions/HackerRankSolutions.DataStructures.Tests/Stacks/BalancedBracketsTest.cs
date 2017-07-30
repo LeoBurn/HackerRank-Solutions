ffe8b14dbe0b0082a1c0ac3048115aba9ec8d836
LeandroViana <leandroviana1988@gmail.com>
2017-07-30 22:52:42 +0100

    Balanced Brackets ( Only missing tests )

---
+++ HackerRankSolutions/HackerRankSolutions.DataStructures.Tests/Stacks/BalancedBracketsTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankSolutions.DataStructures.Tests.Stacks
+{
+    [TestClass]
+    public class BalancedBracketsTest
+    {
+        [TestMethod]
+        public void ShouldReturnIdentifyBalancedBrackets()
+        {
+        }
+    }
+}
---
+++ HackerRankSolutions/HackerRankSolutions.DataStructures/Stacks/BalancedBrackets.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.DataStructures.Stacks
+{
+    public class BalancedBrackets
+    {
+        private static string BALANCED = "YES";
+        private static string NOT_BALANCED = "NO";
+
+        public string[] Run(params string[] values)
+        {
+            string[] results = new string[values.Length];
+            for (int i = 0; i < values.Length; i++)
+            {
+                results[i] = IsBalenced(values[i]) ? BALANCED : NOT_BALANCED;
+            }
+            return results;
+        }
+
+        public static bool IsBalenced(string str)
+        {
+            Stack<char> stack = new Stack<char>();
+            Dictionary<char, char> brackets = new Dictionary<char, char>();
+            brackets.Add('{', '}');
+            brackets.Add('[', ']');
+            brackets.Add('(', ')');
+            foreach (var character in str)
+            {
+                if (brackets.ContainsKey(character))
+                    stack.Push(character);
+
+                else
+                {
+                    var pair = brackets.FirstOrDefault(p => p.Value == character);
+                    if (stack.Count == 0) return false;
+                    var result = stack.Pop();
+                    if (result != pair.Key)
+                        return false;
+
+                }
+            }
+            return stack.Count == 0;
+        }
+    }
+}
