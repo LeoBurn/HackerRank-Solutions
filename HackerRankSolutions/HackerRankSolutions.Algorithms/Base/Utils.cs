3736f3f3126449ffb694a7d61b3d838d30f9e956
LeandroViana <leandroviana1988@gmail.com>
2017-08-06 21:11:14 +0100

    GridLandMetro the problems it's the way a stored values ( BigIntiger it's solution )

--- HackerRankSolutions/HackerRankSolutions.Algorithms/Base/Utils.cs
+++ HackerRankSolutions/HackerRankSolutions.Algorithms/Base/Utils.cs
@@ -11,9 +11,20 @@ namespace HackerRankSolutions.Algorithms.Base
         public void ReadFromStdIn(string[] args)
         {
             string[] tokens_s = Console.ReadLine().Split(' ');
-            int i = Convert.ToInt32(tokens_s[0]);
-            int j = Convert.ToInt32(tokens_s[1]);
-            int k = Convert.ToInt32(tokens_s[2]);
+            int numberOfColumns = Convert.ToInt32(tokens_s[0]);
+            int numberOfLine = Convert.ToInt32(tokens_s[1]);
+            int numberOfTrains = Convert.ToInt32(tokens_s[2]);
+            int[][] grid = new int[numberOfTrains][];
+
+            for (int i = 0; i < numberOfTrains; ++i)
+            {
+                tokens_s = Console.ReadLine().Split(' ');
+                var line = Convert.ToInt32(tokens_s[0]);
+                var begin = Convert.ToInt32(tokens_s[1]);
+                var end = Convert.ToInt32(tokens_s[2]);
+                grid[i] = new int[] {line, begin, end};
+            }
+
         }
     }
 }
--- HackerRankSolutions/HackerRankSolutions.Algorithms/Program.cs
+++ HackerRankSolutions/HackerRankSolutions.Algorithms/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HackerRankSolutions.Algorithms.Implementation;
+using HackerRankSolutions.Algorithms.Search;
 
 namespace HackerRankSolutions.Algorithms
 {
