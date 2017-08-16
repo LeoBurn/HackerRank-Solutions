69e1b310536373abadcac93f4dc374b6409e2eac
LeandroViana <leandroviana1988@gmail.com>
2017-08-16 21:48:05 +0100

    Breaking The Records

---
+++ HackerRankSolutions/HackerRankSolutions.Algorithms.Tests/Implementation/BreakingTheRecordsTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using HackerRankSolutions.Algorithms.Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankSolutions.Algorithms.Tests.Implementation
+{
+    [TestClass]
+    public class BreakingTheRecordsTest
+    {
+        [TestMethod]
+        public void ShouldGetMinAndMax()
+        {
+            //Arrange
+            int[] arr = new[] {10, 5, 20, 20, 4, 5, 2, 25, 1};
+            int n = 9;
+            BreakingTheRecords.Points expected = new BreakingTheRecords.Points() {MaxCounter = 2, MinCounter = 4};
+            BreakingTheRecords runner = new BreakingTheRecords();
+            
+            //Act
+            var result = runner.Run(arr, n);
+            //Assert
+            Assert.AreEqual(result.MinCounter,expected.MinCounter);
+            Assert.AreEqual(result.MaxCounter, expected.MaxCounter);
+        }
+    }
+}
---
+++ HackerRankSolutions/HackerRankSolutions.Algorithms/Implementation/BreakingTheRecords.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.Algorithms.Implementation
+{
+    public class BreakingTheRecords
+    {
+        public class Points
+        {
+            public int MinCounter { get; set; }
+            public int MaxCounter { get; set; }
+        }    
+
+        public Points Run(int[] arr, int n)
+        {
+            int minCounter = 0;
+            int min = arr[0];
+            int maxCounter = 0;
+            int max = arr[0];
+
+            for (int i = 1; i < n; ++i)
+            {
+                var value = arr[i];
+                if (value > max)
+                {
+                    max = value;
+                    maxCounter++;
+                }
+                if (value < min)
+                {
+                    min = value;
+                    minCounter++;
+                }
+            }
+            return new Points() {MaxCounter = maxCounter, MinCounter = minCounter};
+        }
+        
+    }
+}
