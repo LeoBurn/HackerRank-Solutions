e3ad235363091994f56502aeb93f89ef53b125f9
LeandroViana <leandroviana1988@gmail.com>
2017-08-20 11:11:08 +0100

    Day Of Programer

---
+++ HackerRankSolutions/HackerRankSolutions.Algorithms.Tests/Implementation/DayOfProgramerTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using HackerRankSolutions.Algorithms.Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankSolutions.Algorithms.Tests.Implementation
+{
+    /// <summary>
+    /// Summary description for DayOfProgramerTest
+    /// </summary>
+    [TestClass]
+    public class DayOfProgramerTest
+    {
+        [TestMethod]
+        public void ShouldGetDayOfProgramer()
+        {
+            //Arrange
+            int year1 = 2016;
+            string expectedValue1 = "12.09.2016";
+            int year2 = 2017;
+            string expectedValue2 = "13.09.2017";
+            int year3 = 1918;
+            string expectedValue3 = "26.09.1918";
+            DayOfProgramer runner = new DayOfProgramer();
+
+            //Act
+            var result1 = runner.Run(year1);
+            var result2 = runner.Run(year2);
+            var result3 = runner.Run(year3);
+
+            //Assert
+            Assert.AreEqual(expectedValue1,result1);
+            Assert.AreEqual(expectedValue2, result2);
+            Assert.AreEqual(expectedValue3, result3);
+        }
+    }
+}
---
+++ HackerRankSolutions/HackerRankSolutions.Algorithms/Implementation/DayOfProgramer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.Algorithms.Implementation
+{
+    public class DayOfProgramer
+    {
+        public string Run(int year)
+        {
+            int month = 9;
+            int day;
+            int fevDays;
+            
+            fevDays = (year%4 == 0) ? 29 : 28;
+            if (1918 == year)   
+            {
+                fevDays = 15;
+            }
+            if (1918 < year)
+                fevDays = ((year%400 == 0) || (year%4 == 0 && year%100 != 0)) ? 29 : 28;
+
+            int tempValues = 31 + fevDays + 31 + 30 + 31 + 30 + 31 + 31;
+            day = 256 - tempValues;
+
+            return $"{day}.{ String.Format("{0:D2}", month) }.{year}";
+
+        }
+    }
+}
