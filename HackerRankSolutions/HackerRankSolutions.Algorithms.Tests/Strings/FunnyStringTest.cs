27076a2e097c67a93f50a0616e9b5f2ca1c33d51
LeandroViana <leandroviana1988@gmail.com>
2017-08-24 23:47:37 +0100

    Funny String

---
+++ HackerRankSolutions/HackerRankSolutions.Algorithms.Tests/Strings/FunnyStringTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using HackerRankSolutions.Algorithms.Strings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankSolutions.Algorithms.Tests.Strings
+{
+    /// <summary>
+    /// Summary description for FunnyStringTest
+    /// </summary>
+    [TestClass]
+    public class FunnyStringTest
+    {
+
+        [TestMethod]
+        public void ShouldGetFunnyString()
+        {
+            //Arrange
+            string s = "acxz";
+            string exptectedValue = "Funny";
+            FunnyString runner = new FunnyString();
+
+            //Act
+            var result = runner.Run(s);
+
+            //Assert
+            Assert.AreEqual(exptectedValue,result);
+        }
+
+        [TestMethod]
+        public void ShouldGetNotFunnyString()
+        {
+            //Arrange
+            string s = "bcxz";
+            string exptectedValue = "Not Funny";
+            FunnyString runner = new FunnyString();
+
+            //Act
+            var result = runner.Run(s);
+
+            //Assert
+            Assert.AreEqual(exptectedValue, result);
+        }
+    }
+}
---
+++ HackerRankSolutions/HackerRankSolutions.Algorithms/Strings/FunnyString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.Algorithms.Strings
+{
+    public class FunnyString
+    {
+        public string Run(string s)
+        {
+
+            for(int beginIdx = 1, endIdx = s.Length - 2; beginIdx < s.Length && endIdx >= 0;beginIdx++,endIdx--)
+            {
+                var firstValue = Math.Abs(s[beginIdx] - s[beginIdx - 1]);
+                var lastValue = Math.Abs(s[endIdx] - s[endIdx + 1]);
+
+                if (firstValue != lastValue)
+                    return "Not Funny";
+                
+                
+
+            }
+            return "Funny";
+        }
+    }
+}
