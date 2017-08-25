c2ccf0051ea19b0cfe4815c94290dfcff51af634
LeandroViana <leandroviana1988@gmail.com>
2017-08-25 15:06:08 +0100

    Between Two Sets

---
+++ HackerRankSolutions/HackerRankSolutions.Algorithms.Tests/Implementation/BetweenTwoSetsTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using HackerRankSolutions.Algorithms.Implementation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankSolutions.Algorithms.Tests.Implementation
+{
+    /// <summary>
+    /// Summary description for BetweenTwoSetsTest
+    /// </summary>
+    [TestClass]
+    public class BetweenTwoSetsTest
+    {
+       
+
+        [TestMethod]
+        public void ShouldReturnTwoSubSetANotContainsAnyElement()
+        {
+            //Arrange
+            var subsetA = new int[] {3, 9, 6};
+            var subsetB = new int[] {36,72};
+            var expectedValue = 2;
+            BetweenTwoSets runner = new BetweenTwoSets();
+
+            //Act
+            var result = runner.Run(subsetA, subsetB);
+
+            //Assert
+            Assert.AreEqual(expectedValue,result);
+        }
+    }
+}
---
+++ HackerRankSolutions/HackerRankSolutions.Algorithms/Implementation/BetweenTwoSets.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.Algorithms.Implementation
+{
+    public class BetweenTwoSets
+    {
+        public int Run(int[] a, int[] b)
+        {
+            var maxElementInA = a.Max();
+            var xValues = maxElementInA;
+            var elementCounter = 0;
+
+            xValues = lcm(a);
+
+            for (; xValues <= b[0]; xValues += maxElementInA)
+            {
+                if (b.All(p => p % xValues == 0))
+                    elementCounter++;
+            }
+
+            return elementCounter;
+        }
+
+        private static int gcd(int a, int b)
+        {
+            while (b > 0)
+            {
+                int temp = b;
+                b = a % b; // % is remainder
+                a = temp;
+            }
+            return a;
+        }
+
+        private static int gcd(int[] input)
+        {
+            int result = input[0];
+            for (int i = 1; i < input.Length; i++)
+            {
+                result = gcd(result, input[i]);
+            }
+            return result;
+        }
+
+        private static int lcm(int a, int b)
+        {
+            return a * (b / gcd(a, b));
+        }
+
+        private static int lcm(int[] input)
+        {
+            int result = input[0];
+            for (int i = 1; i < input.Length; i++)
+            {
+                result = lcm(result, input[i]);
+            }
+            return result;
+        }
+    }
+}
