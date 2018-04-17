d30f4b6707ec5ec89b1187f68bed2c9abeef36c1
CELFINET\leandro.g.viana <leandro.g.viana@celfinet.com>
2018-04-17 14:42:45 +0100

    2 Problem Week Of Code 37

---
+++ HackerRankSolutions/HackerRankSolutions.WeekOfCode37.Tests/SimpleLanguageTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankSolutions.WeekOfCode37.Tests
+{
+    [TestClass]
+    public class SimpleLanguageTest
+    {
+        [TestMethod]
+        public void FirstTestCenario()
+        {
+            //Arrange
+            int numberOfLines = 3;
+            string[] lines = { "add 2","set 1","add -1" };
+            double expectedValue = 2;
+            SimpleLanguage runner = new SimpleLanguage();
+
+            //Act
+            var result = runner.Run(lines, numberOfLines);
+
+            //Assert
+            Assert.AreEqual(result, expectedValue);
+        }
+
+        [TestMethod]
+        public void SecondTestCenario()
+        {
+            //Arrange
+            int numberOfLines = 2;
+            string[] lines = { "set 5", "add 6"};
+            double expectedValue = 11;
+            SimpleLanguage runner = new SimpleLanguage();
+
+            //Act
+            var result = runner.Run(lines, numberOfLines);
+
+            //Assert
+            Assert.AreEqual(result, expectedValue);
+        }
+
+        [TestMethod]
+        public void ThirdTestCenario()
+        {
+            //Arrange
+            int numberOfLines = 3;
+            string[] lines = { "add -1", "set -3","add -2" };
+            double expectedValue = 0;
+            SimpleLanguage runner = new SimpleLanguage();
+
+            //Act
+            var result = runner.Run(lines, numberOfLines);
+
+            //Assert
+            Assert.AreEqual(result, expectedValue);
+        }
+
+        [TestMethod]
+        public void FourthTestCenario()
+        {
+            //Arrange
+            int numberOfLines = 5;
+            string[] lines = { "add 2", "add 3", "set 7","add -1","add 2" };
+            double expectedValue = 9;
+            SimpleLanguage runner = new SimpleLanguage();
+
+            //Act
+            var result = runner.Run(lines, numberOfLines);
+
+            //Assert
+            Assert.AreEqual(result, expectedValue);
+        }
+    }
+}
---
+++ HackerRankSolutions/HackerRankSolutions.WeekOfCode37/SimpleLanguage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.WeekOfCode37
+{
+    public class SimpleLanguage
+    {
+        public long Run(string[] lines , int numeOfLines)
+        {
+            long actualValue = 0;
+            long max = 0;
+            for (int i = 0; i < numeOfLines; ++i)
+            {
+                var line = lines[i];
+
+                var operations = line.Split(' ');
+                if (operations[0].Equals("add", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    var value = long.Parse(operations[1]);
+                    if (value > 0)
+                        actualValue += value;
+
+                }
+                if (operations[0].Equals("set", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    var value = long.Parse(operations[1]);
+                    if (value > actualValue)
+                        actualValue = long.Parse(operations[1]);
+                }
+
+                if (actualValue > max)
+                    max = actualValue;
+            }
+            return max;
+        }
+    }
+}
