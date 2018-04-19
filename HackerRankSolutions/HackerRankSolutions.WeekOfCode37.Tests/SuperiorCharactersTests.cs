33a5f2bbb2d74fbdb1503104df9c4a32943e4925
CELFINET\leandro.g.viana <leandro.g.viana@celfinet.com>
2018-04-19 01:44:10 +0100

    3 Problem of WeefOfCode 37

---
+++ HackerRankSolutions/HackerRankSolutions.WeekOfCode37.Tests/SuperiorCharactersTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankSolutions.WeekOfCode37.Tests
+{
+    [TestClass]
+    public class SuperiorCharactersTests
+    {
+        [TestMethod]
+        public void SuperiorCharactersFirstTestCase()
+        {
+            //Arrange
+            int[] words = new int[] { 0,0,0,0,2,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0,0,0,0,0,0,0};
+            SuperiorCharacters runner = new SuperiorCharacters();
+            int expectedValue = 1;
+
+            //Act
+            var result = runner.MaximumSuperiorCharacters(words, 1);
+
+            //Assert
+            Assert.AreEqual(expectedValue,result);
+        }
+
+        [TestMethod]
+        public void SuperiorCharactersSecondTestCase()
+        {
+            //Arrange
+            int[] words = new int[] { 1, 2, 2, 3, 4, 0, 3, 4, 4, 1, 3, 1, 4, 4, 1, 0, 0, 0, 0, 0, 4, 2, 3, 2, 2, 1 };
+            SuperiorCharacters runner = new SuperiorCharacters();
+            int expectedValue = 25;
+
+            //Act
+            var result = runner.MaximumSuperiorCharacters(words, 1);
+
+            //Assert
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        [TestMethod]
+        public void SuperiorCharactersThirdTestCase()
+        {
+            //Arrange
+            int[] words = new int[] { 1, 1, 3, 3, 1, 1, 4, 4, 3, 1, 3, 3, 3, 0, 1, 2, 0, 4, 2, 1, 3, 0, 3, 1, 1, 1 };
+            SuperiorCharacters runner = new SuperiorCharacters();
+            int expectedValue = 24;
+
+            //Act
+            var result = runner.MaximumSuperiorCharacters(words, 1);
+
+            //Assert
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        [TestMethod]
+        public void SuperiorCharactersFourthTestCase()
+        {
+            //Arrange
+            int[] words = new int[] { 2, 1, 4, 1, 0, 2, 0, 3, 1, 2, 0, 3, 1, 1, 2, 0, 1, 4, 2, 3, 2, 3, 2, 0, 2, 1 };
+            SuperiorCharacters runner = new SuperiorCharacters();
+            int expectedValue = 21;
+
+            //Act
+            var result = runner.MaximumSuperiorCharacters(words, 1);
+
+            //Assert
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        [TestMethod]
+        public void SuperiorCharactersFifthTestCase()
+        {
+            //Arrange
+            int[] words = new int[] { 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            SuperiorCharacters runner = new SuperiorCharacters();
+            int expectedValue = 0;
+
+            //Act
+            var result = runner.MaximumSuperiorCharacters(words, 1);
+
+            //Assert
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        [TestMethod]
+        public void SuperiorCharactersSixtTestCase()
+        {
+            //Arrange
+            int[] words = new int[] { 15, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            SuperiorCharacters runner = new SuperiorCharacters();
+            int expectedValue = 7;
+
+            //Act
+            var result = runner.MaximumSuperiorCharacters(words, 1);
+
+            //Assert
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        [TestMethod]
+        public void SuperiorCharactersSeventhTestCase()
+        {
+            //Arrange
+            int[] words = new int[] { 1, 3, 8, 1, 2, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            SuperiorCharacters runner = new SuperiorCharacters();
+            int expectedValue = 7;
+
+            //Act
+            var result = runner.MaximumSuperiorCharacters(words, 1);
+
+            //Assert
+            Assert.AreEqual(expectedValue, result);
+        }
+    }
+}
---
+++ HackerRankSolutions/HackerRankSolutions.WeekOfCode37/SuperiorCharacters.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.WeekOfCode37
+{
+    public class SuperiorCharacters
+    {
+        public void Run(int[][] matrix , int numberOfLines)
+        {
+            
+        }
+
+        public long MaximumSuperiorCharacters(int[] words, int numberOfLines)
+        {
+            int totalChars = words.Sum(p => p);
+            int numberOfCombinations = ((int)Math.Round((totalChars / (double)2), 0, MidpointRounding.AwayFromZero))-1 ;
+            int numberOfDiferentChars = words.Count(p => p > 0);
+            int morechars = words.Max(p => p);
+
+            if (numberOfDiferentChars <= 1 && totalChars <= 2)
+                return 0;
+
+            if ((totalChars / 2) < morechars)
+            {
+                var mid = totalChars / 2;
+                var diffMidToMoreChars = morechars - mid;
+                return numberOfCombinations - diffMidToMoreChars;
+            }
+
+            if (numberOfDiferentChars == 2)
+            {
+                int left = words.First(p => p > 0);
+                int right = words.Last(p => p > 0);
+                if (left <= right)
+                    return left - 1;
+                return right;
+            }
+
+
+            return numberOfCombinations;
+        }
+
+
+   
+
+
+        //*************************************************************************
+        //This is CopyWrite Code Not Mine
+        //https://stackoverflow.com/questions/26311984/permutation-and-combination-in-c-sharp
+        public long nCr(int n, int r)
+        {
+            // naive: return Factorial(n) / (Factorial(r) * Factorial(n - r));
+            return nPr(n, r) / Factorial(r);
+        }
+
+        public static long nPr(int n, int r)
+        {
+            // naive: return Factorial(n) / Factorial(n - r);
+            return FactorialDivision(n, n - r);
+        }
+
+        private static long FactorialDivision(int topFactorial, int divisorFactorial)
+        {
+            long result = 1;
+            for (int i = topFactorial; i > divisorFactorial; i--)
+                result *= i;
+            return result;
+        }
+
+        private  long Factorial(int i)
+        {
+            if (i <= 1)
+                return 1;
+            return i * Factorial(i - 1);
+        }
+
+        //******************************************************************************
+    }
+}
