b3af9b445cb846da5eb21f8115c858dc8b6d73ef
CELFINET\leandro.g.viana <leandro.g.viana@celfinet.com>
2018-05-23 16:17:41 +0100

    Cracking The Coding Interview - MakingAnagram

---
+++ HackerRankSolutions/HackerRankSolutions.CrakingTheCodingInterview.Tests/MakingAnagramsTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankSolutions.CrakingTheCodingInterview.Tests
+{
+    [TestClass]
+    public class MakingAnagramsTest
+    {
+        [TestMethod]
+        public void TestCase1Anagram()
+        {
+            //Arrange 
+            string s1 = "cde";
+            string s2 = "abc";
+            MakingAnagrams runner = new MakingAnagrams();
+            int expectedValue = 4;
+
+            //Act
+            var result = runner.Run(s1, s2);
+
+            //Assert
+            Assert.AreEqual(expectedValue, result);
+        }
+    }
+}
---
+++ HackerRankSolutions/HackerRankSolutions.CrakingTheCodingInterview/MakingAnagrams.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.CrakingTheCodingInterview
+{
+    public class MakingAnagrams
+    {
+        //O(N) + O(N) + O(26) = O(N)
+        public int Run(string s1,string s2)
+        {
+            var alphabet = new int?[26];
+            s1 = s1.ToLower();
+            s2 = s2.ToLower();
+
+            foreach(var ch in s1)
+            {
+                var idx = ch - 'a';
+                var value = alphabet[idx] == null ? 1 : ++alphabet[idx];
+                alphabet[idx] = value;
+            }
+
+            foreach (var ch in s2)
+            {
+                var idx = ch - 'a';
+                var value = alphabet[idx] == null ? -1 : --alphabet[idx];
+                alphabet[idx] = value;
+            }
+
+            return alphabet.Sum(number => Math.Abs(number ?? 0));
+        }
+    }
+}
