b54f57a446a7d74887c2902d8dfd3407fe1cc869
CELFINET\leandro.g.viana <leandro.g.viana@celfinet.com>
2018-05-30 16:55:31 +0100

    CrakingTheCodingInterview: QueuesATaleOfTwoStacks

---
+++ HackerRankSolutions/HackerRankSolutions.CrakingTheCodingInterview.Tests/QueuesATaleOfTwoStacksTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankSolutions.CrakingTheCodingInterview.Tests
+{
+    [TestClass]
+    public class QueuesATaleOfTwoStacksTest
+    {
+        [TestMethod]
+        public void TestCase1QueuesATaleOfTwoStacks()
+        {
+            //Arrange 
+            int[] operations = {1, 2, 1, 3, 1, 3, 1, 1, 2, 2};
+            int?[] values = {42, null, 14, null, 28, null, 60, 78, null, null};
+            int?[] expectedValues = {null, null, null, 14, null, 14, null, null, null, null};
+            
+            QueuesATaleOfTwoStacks runner = new QueuesATaleOfTwoStacks();
+
+            //Act
+            var result = runner.Run(operations, values);
+
+            //Assert
+            CollectionAssert.AreEqual(expectedValues,result);
+        }
+    }
+}
---
+++ HackerRankSolutions/HackerRankSolutions.CrakingTheCodingInterview/QueuesATaleOfTwoStacks.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.CrakingTheCodingInterview
+{
+    public class QueuesATaleOfTwoStacks
+    {
+        public Queue<int> _queue;
+
+        public QueuesATaleOfTwoStacks()
+        {
+            _queue = new Queue<int>();
+        }
+
+        public int?[] Run(int[] operation, int?[] value)
+        {
+            int?[] result = new int?[operation.Length];
+            for (int i = 0; i < operation.Length; ++i)
+            {
+                result[i] = RunSingle(operation[i], value[i]);
+            }
+
+            return result;
+        }
+
+        public int? RunSingle(int operation, int? value)
+        {
+            switch (operation)
+            {
+                case 1:
+                    if (value != null) _queue.Enqueue(value.Value);
+                    break;
+                case 2:
+                    _queue.Dequeue();
+                    break;
+                case 3:
+                    var element = _queue.FirstOrDefault();
+                    return element;
+            }
+
+            return null;
+        }
+    }
+}
