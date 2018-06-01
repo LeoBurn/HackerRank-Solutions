7a6aa46f57524b622cbaa1988e9f6de038e53f25
CELFINET\leandro.g.viana <leandro.g.viana@celfinet.com>
2018-06-01 15:13:47 +0100

    HackerRankSolutions.CrakingTheCodingInterview - TreesIsThisABinarySearchTree

---
+++ HackerRankSolutions/HackerRankSolutions.CrakingTheCodingInterview.Tests/TreesIsThisABinarySearchTreeTests.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HackerRankSolutions.CrakingTheCodingInterview;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRankSolutions.CrakingTheCodingInterview.Tests
+{ 
+
+    [TestClass]
+    public class TreesIsThisABinarySearchTreeTests
+    {
+        [TestMethod]
+        public void TreesIsThisABinarySearchTreeTestCase1()
+        {
+            //Arrange
+            TreesIsThisABinarySearchTree runner = new TreesIsThisABinarySearchTree();
+            Node root = new Node() {Data = 4 , Left = new Node()
+                                                {
+                                                    Data = 2,
+                                                    Left = new Node(){ Data = 1} ,
+                                                    Right = new Node(){Data = 3}},
+                                                Right = new Node()
+                                                {
+                                                    Data = 6,
+                                                    Left = new Node() { Data = 5},
+                                                    Right = new Node() { Data = 7}
+                                                }
+            };
+            var expectedValue = true;
+            
+            //Act
+            var result = runner.Run(root);
+
+            //Assert
+            Assert.AreEqual(expectedValue, result);
+        }
+
+        [TestMethod]
+        public void TreesIsThisABinarySearchTreeTestCase2()
+        {
+            //Arrange
+            TreesIsThisABinarySearchTree runner = new TreesIsThisABinarySearchTree();
+            Node root = new Node()
+            {
+                Data = 4,
+                Left = new Node()
+                {
+                    Data = 3,
+                    Left = new Node() { Data = 2 },
+                    Right = new Node() { Data = 3 }
+                },
+                Right = new Node()
+                {
+                    Data = 6,
+                    Left = new Node() { Data = 5 },
+                    Right = new Node() { Data = 7 }
+                }
+            };
+            var expectedValue = false;
+
+            //Act
+            var result = runner.Run(root);
+
+            //Assert
+            Assert.AreEqual(expectedValue, result);
+        }
+    }
+}
---
+++ HackerRankSolutions/HackerRankSolutions.CrakingTheCodingInterview/TreesIsThisABinarySearchTree.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions.CrakingTheCodingInterview
+{
+    public class TreesIsThisABinarySearchTree
+    {
+        public bool Run(Node root)
+        {
+            return IsBST(root,Int32.MinValue, Int32.MaxValue);
+        }
+
+        public bool IsBST(Node root, int min,int max)
+        {
+            if (root == null)
+                return true;
+
+            if (root.Data < min || root.Data > max)
+                return false;
+
+            return (IsBST(root.Left, min, root.Data - 1) &&
+                    IsBST(root.Right, root.Data + 1, max));
+        }
+    }
+
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Left { get; set; }
+        public Node Right { get; set; }
+    }
+}
